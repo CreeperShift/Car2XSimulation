2c4ad1e0f4a882ec85cf1d6e9e08b34a9e4a4e65
CreeperShift <henkes@stud.fra-uas.de>
2021-01-06 09:37:11 +0100

    Simulation speed control

--- Main.go
+++ Main.go
@@ -3,51 +3,66 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
-	"github.com/faiface/pixel/text"
 	"golang.org/x/image/colornames"
-	"golang.org/x/image/font/basicfont"
+	"time"
 )
 
 var mainWindow *pixelgl.Window
 var streetMap *StreetMap
-var basicAtlas *text.Atlas
 
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Car2Car Simulation",
 		Bounds: pixel.R(0, 0, 930, 930),
-		VSync:  true,
+		VSync:  false,
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
 	mainWindow = win
-	basicAtlas = text.NewAtlas(basicfont.Face7x13, text.ASCII)
 	Init()
 	win.Clear(colornames.Black)
 
+	last := time.Now()
+	dt := 0.5
 	for !win.Closed() {
 		win.Update()
-		update()
+		dt = dt + time.Since(last).Seconds()
+		last = time.Now()
+		if dt > 0.5 {
+			dt = 0.0
+			update()
+		}
 	}
 }
 
 func Init() {
 	streetMap = NewMap(30, 31)
 	streetMap.addStreets()
-	streetMap.addCar()
-	//TODO: spawn obstacle (1 or more)
+	streetMap.addCars(10)
+	streetMap.addObstacles(3, 15)
 }
 
 func update() {
 	streetMap.renderMap()
 	streetMap.MoveCars()
 	streetMap.RenderCars()
+	renderButtons()
 	//TODO: Check Obstacles
 	//TODO: Communication
 	//TODO: Warn
 	//TODO: Render Comm/Warning
+}
+
+func renderButtons() {
+	sFaster := LoadAndSprite("assets/bFaster.png")
+	sSlower := LoadAndSprite("assets/bSlower.png")
+	mat := pixel.IM
+
+	sFaster.Draw(mainWindow, mat)
+	sSlower.Draw(mainWindow, mat)
+
 }
 
 func main() {
